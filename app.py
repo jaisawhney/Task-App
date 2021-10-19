06071258387c557d2563d2d7eea8ac222fba7c3a
Jai Sawhney <jai.sawhney1@gmail.com>
2021-10-19 12:06:47 -0700

    Html skeleton

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
 db = SQLAlchemy(app)
+
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,7 +15,8 @@ class Todo(db.Model):
     def __repr__(self):
         return f"<Task {id}>"
 
-@app.route("/")
+
+@app.route("/", methods=["POST", "GET"])
 def index():
     return render_template("index.html")
 
